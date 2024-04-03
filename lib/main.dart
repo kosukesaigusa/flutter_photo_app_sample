552dcc2adc9cb428dabd5e816776d7c0193449e6
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2024-04-04 00:02:50 +0900

    feat: update photo sample app

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,13 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:gap/gap.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
   runApp(const ProviderScope(child: MaterialApp(home: MainApp())));
 }
+
+enum ImageSize { medium, large }
 
 class MainApp extends HookWidget {
   const MainApp({super.key});
@@ -15,6 +16,8 @@ class MainApp extends HookWidget {
   @override
   Widget build(BuildContext context) {
     final count = useState<int>(5);
+    final optimize = useState<bool>(false);
+    final imageSize = useState<ImageSize>(ImageSize.medium);
     return Scaffold(
       appBar: AppBar(
         title: Text('表示件数：${count.value} 件'),
@@ -28,7 +31,9 @@ class MainApp extends HookWidget {
         itemCount: count.value,
         itemBuilder: (context, index) {
           final imageUrl =
-              'https://photo-app-sample-image-worker.saigusa758cloudy.workers.dev/${(index + 1).toString().padLeft(3, '0')}.JPG';
+              'https://photo-app-sample-image-worker.saigusa758cloudy.workers.dev'
+              '/${imageSize.value.name}'
+              '/${(index + 1).toString().padLeft(3, '0')}.JPG?optimize=${optimize.value}';
           return CachedNetworkImage(
             imageUrl: imageUrl,
             imageBuilder: (context, imageProvider) => AspectRatio(
@@ -57,6 +62,50 @@ class MainApp extends HookWidget {
         child: Row(
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
+            SegmentedButton<ImageSize>(
+              segments: const [
+                ButtonSegment<ImageSize>(
+                  value: ImageSize.medium,
+                  label: Text('中'),
+                ),
+                ButtonSegment<ImageSize>(
+                  value: ImageSize.large,
+                  label: Text('大'),
+                ),
+              ],
+              selected: {imageSize.value},
+              onSelectionChanged: (value) {
+                imageSize.value = value.first;
+                ScaffoldMessenger.of(context)
+                  ..removeCurrentSnackBar()
+                  ..showSnackBar(
+                    SnackBar(
+                      content: Text('画像サイズを ${value.first.name} に変更しました。'),
+                    ),
+                  );
+              },
+            ),
+            const VerticalDivider(),
+            Switch(
+              value: optimize.value,
+              onChanged: (value) {
+                optimize.value = value;
+                if (value) {
+                  ScaffoldMessenger.of(context)
+                    ..removeCurrentSnackBar()
+                    ..showSnackBar(
+                      const SnackBar(content: Text('画像サイズ・クオリティの最適化を有効にしました。')),
+                    );
+                } else {
+                  ScaffoldMessenger.of(context)
+                    ..removeCurrentSnackBar()
+                    ..showSnackBar(
+                      const SnackBar(content: Text('画像サイズ・クオリティの最適化を無効にしました。')),
+                    );
+                }
+              },
+            ),
+            const VerticalDivider(),
             IconButton(
               onPressed: () {
                 if (count.value > 0) {
@@ -73,7 +122,6 @@ class MainApp extends HookWidget {
                 Icons.remove,
               ),
             ),
-            const Gap(8),
             IconButton(
               onPressed: () {
                 if (count.value < 100) {
@@ -88,7 +136,7 @@ class MainApp extends HookWidget {
               },
               icon: const Icon(Icons.add),
             ),
-            const Gap(32),
+            const VerticalDivider(),
             IconButton(
               onPressed: () async {
                 await DefaultCacheManager().emptyCache();
