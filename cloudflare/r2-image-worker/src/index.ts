043aa6a8d9e8998a0ba52daf247f866ea166ffb7
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2024-04-04 00:02:39 +0900

    feat: update image worker

--- cloudflare/r2-image-worker/src/index.ts
+++ cloudflare/r2-image-worker/src/index.ts
@@ -2,6 +2,7 @@ import { cache } from 'hono/cache'
 import { logger } from 'hono/logger'
 import { Hono } from 'hono/quick'
 import { optimizeImage } from 'wasm-image-optimization'
+import { arrayBufferToBase64, detectType } from './utils'
 
 type Bindings = {
   BUCKET: R2Bucket
@@ -22,16 +23,19 @@ app.get(
   })
 )
 
-app.get('/:fileName', async (c) => {
-  const optimize = c.req.query('optimize')
+app.get(':dirName/:fileName', async (c) => {
+  const optimize = c.req.query('optimize') === 'true'
+  const dirName = c.req.param().dirName
   const fileName = c.req.param().fileName
 
-  const object = await c.env.BUCKET.get(fileName)
+  const object = await c.env.BUCKET.get(`${dirName}/${fileName}`)
   if (!object) return c.notFound()
 
-  const contentType = object.httpMetadata?.contentType ?? ''
-
   const buffer = await object.arrayBuffer()
+
+  const type = detectType(arrayBufferToBase64(buffer))
+  if (!type) return c.notFound()
+
   let optimized: Uint8Array | null = null
   if (optimize) {
     optimized = await optimizeImage({
@@ -40,9 +44,11 @@ app.get('/:fileName', async (c) => {
       width: 300,
       // 調整する
       quality: 75,
+      format: 'jpeg',
     })
   }
 
+  const contentType = object.httpMetadata?.contentType ?? ''
   return c.body(optimized ?? buffer, 200, {
     'Cache-Control': `public, max-age=${maxAge}`,
     'Content-Type': contentType,
--- cloudflare/r2-image-worker/src/utils.ts
+++ cloudflare/r2-image-worker/src/utils.ts
@@ -1,20 +1,30 @@
 export type Type = {
   mimeType: string
-  suffix: string
+  suffix: 'png' | 'jpeg' | 'webp'
 }
 
 const signatures: Record<string, Type> = {
-  R0lGODdh: { mimeType: 'image/gif', suffix: 'gif' },
-  R0lGODlh: { mimeType: 'image/gif', suffix: 'gif' },
+  // R0lGODdh: { mimeType: 'image/gif', suffix: 'gif' },
+  // R0lGODlh: { mimeType: 'image/gif', suffix: 'gif' },
   iVBORw0KGgo: { mimeType: 'image/png', suffix: 'png' },
-  '/9j/': { mimeType: 'image/jpg', suffix: 'jpg' },
+  '/9j/': { mimeType: 'image/jpg', suffix: 'jpeg' },
   UklGR: { mimeType: 'image/webp', suffix: 'webp' },
 }
 
-export const detectType = (b64: string): Type | undefined => {
+export function detectType(b64: string): Type | undefined {
   for (const s in signatures) {
     if (b64.indexOf(s) === 0) {
       return signatures[s]
     }
   }
+}
+
+export function arrayBufferToBase64(buffer: ArrayBuffer): string {
+  let binary = ''
+  const bytes = new Uint8Array(buffer)
+  const len = bytes.byteLength
+  for (let i = 0; i < len; i++) {
+    binary += String.fromCharCode(bytes[i])
+  }
+  return btoa(binary)
 }
